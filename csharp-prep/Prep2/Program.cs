185a278108fc7aa80c77d55907069cd78fc15315
SethMcNabb <youremail@byui.edu>
2023-11-04 16:11:29 -0600

    Worked prep 1-5

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradeString = Console.ReadLine();
+    
+        int gradeInt = int.Parse(gradeString);
+
+        string letter;
+
+        if (gradeInt >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeInt >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeInt >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeInt >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradeInt < 50)
+        {
+            letter = "F";
+        }
+        else 
+        {
+            letter = "invalid option";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You did not pass.");
+        }
     }
 }
