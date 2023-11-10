806b6e29edf14497fab890089fd16b2dad008d31
SethMcNabb <youremail@byui.edu>
2023-11-09 22:40:12 -0700

    Worked on prep 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Project Manager";
+        job1._company = "Conservice";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Facebook";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Gary Clark";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
