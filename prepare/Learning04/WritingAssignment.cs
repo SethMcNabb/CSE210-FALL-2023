560ae323a86a45bffdf84b74b82fe27805fc5e2d
SethMcNabb <youremail@byui.edu>
2023-12-02 15:49:34 -0700

    Found I was pushing wrong folder.

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,15 @@
+class WritingAssignment : Assignment
+{
+    private string _writingInformation;
+
+    public WritingAssignment(string studentName, string topic, string writingInformation)
+        : base(studentName, topic)
+    {
+        _writingInformation = writingInformation;
+    }
+
+    public string GetWritingInformation()
+    {
+        return _writingInformation;
+    }
+}
